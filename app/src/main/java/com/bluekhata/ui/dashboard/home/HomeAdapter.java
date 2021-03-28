641b900e1d326da3500e42532c88d038951c73c9
DESKTOP-8PV3A9L\anand <anandg@ubic.com>
2021-03-28 23:51:10 +0530

    added limit on home adapter upto five

--- app/src/main/java/com/bluekhata/ui/dashboard/home/HomeAdapter.java
+++ app/src/main/java/com/bluekhata/ui/dashboard/home/HomeAdapter.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 
 import com.bluekhata.R;
 import com.bluekhata.data.model.db.custom.TransactionWithCategory;
+import com.bluekhata.databinding.ItemHomeMoreBinding;
 import com.bluekhata.databinding.ItemHomeTransactionBinding;
 import com.bluekhata.ui.base.BaseViewHolder;
 import com.bluekhata.ui.dashboard.RefreshListOnDismiss;
@@ -22,12 +23,16 @@ import java.util.List;
  * Created by aman on 05-08-2018.
  */
 
-public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
+public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private FragmentActivity context;
     private RefreshListOnDismiss listener;
     private long selectedStartDate, selectedEndDate;
     private ArrayList<TransactionWithCategory> list;
+
+    private static final int ITEM_HOME = 1;
+    private static final int ITEM_MORE = 2;
+    private static final int ITEMS_COUNT = 5;
 
     public HomeAdapter(FragmentActivity context) {
         this.context = context;
@@ -36,19 +41,31 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
 
     @NonNull
     @Override
-    public HomeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == ITEM_MORE) {
+            ItemHomeMoreBinding bindingMore = ItemHomeMoreBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+            return new ButtonViewHolder(bindingMore);
+        }
         ItemHomeTransactionBinding binding = ItemHomeTransactionBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
         return new ViewHolder(binding);
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HomeAdapter.ViewHolder holder, int position) {
-        holder.onBind(position);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ViewHolder){
+            ((ViewHolder)holder).onBind(position);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return list == null ? 0 : list.size();
+        return list == null ? 0 : list.size() > ITEMS_COUNT ? ITEMS_COUNT + 1 : list.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position >= ITEMS_COUNT ? ITEM_MORE : ITEM_HOME;
     }
 
     public void refreshList(List<TransactionWithCategory> list) {
@@ -87,6 +104,26 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
             long categoryId = list.get(getAdapterPosition()).getTransaction().getCategoryId();
             HomeDetailFragment homeDetailFragment = HomeDetailFragment.getInstance(selectedStartDate, selectedEndDate, categoryId);
             AppUtils.setFragment(homeDetailFragment,true, context, R.id.container, HomeDetailFragment.TAG);
+
+        }
+    }
+
+    public class ButtonViewHolder extends BaseViewHolder implements View.OnClickListener {
+        private ItemHomeMoreBinding binding;
+
+        public ButtonViewHolder(ItemHomeMoreBinding itemView) {
+            super(itemView.getRoot());
+            binding = itemView;
+
+            itemView.getRoot().setOnClickListener(this);
+        }
+
+        @Override
+        public void onBind(int position) {
+        }
+
+        @Override
+        public void onClick(View view) {
 
         }
     }
